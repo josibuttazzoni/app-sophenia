9e61b7d10b23ca7f57217f86ad06b83292520e71
Martina Mattioli <martinamattioli0@gmail.com>
2024-10-27 14:51:57 -0300

    feat(table): add pagination

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,18 +1,53 @@
-import { ReactNode } from 'react';
+import { ReactNode, useState } from 'react';
+import { Employee } from 'src/types/employee';
+import { Task } from 'src/types/tasks';
 
-import { Table, TableBody, TableHead, TableHeader, TableRow } from '#components/ui/table';
+import { Pagination, PaginationContent, PaginationItem, PaginationLink } from '#components/ui/pagination';
+import { TableRow } from '#components/ui/table';
 
-export function BaseTable({ columns, children }: { columns: string[]; children: ReactNode }) {
+import { BaseTable } from './BaseTable';
+import { ITEMS_PER_PAGE } from './constants';
+
+type TableData = Task | Employee;
+
+type PaginatedTableWrapperProps<T extends TableData> = {
+  data: T[];
+  columns: string[];
+  itemsPerPage?: number;
+  row: (item: T) => JSX.Element | ReactNode;
+};
+
+export default function PaginatedTableWrapper<T extends TableData>({
+  data,
+  columns,
+  itemsPerPage = ITEMS_PER_PAGE,
+  row
+}: PaginatedTableWrapperProps<T>) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const paginatedData = data.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
+
+  const handlePageChange = (page: number) => setCurrentPage(page);
+
   return (
-    <Table>
-      <TableHeader>
-        <TableRow className="hover:bg-transparent">
-          {columns.map(column => (
-            <TableHead key={column}>{column}</TableHead>
+    <div className="flex h-full flex-col justify-between">
+      <BaseTable columns={columns}>
+        {paginatedData.map((item, index) => (
+          <TableRow key={index}>{row(item)}</TableRow>
+        ))}
+      </BaseTable>
+      <Pagination>
+        <PaginationContent>
+          {Array.from({ length: totalPages }, (_, i) => (
+            <PaginationItem key={i}>
+              <PaginationLink isActive={i + 1 === currentPage} onClick={() => handlePageChange(i + 1)}>
+                {i + 1}
+              </PaginationLink>
+            </PaginationItem>
           ))}
-        </TableRow>
-      </TableHeader>
-      <TableBody>{children}</TableBody>
-    </Table>
+        </PaginationContent>
+      </Pagination>
+    </div>
   );
 }
