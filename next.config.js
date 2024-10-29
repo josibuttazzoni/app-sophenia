ef165c737586ed75df2b790c7fe4ecce652cd224
Juan Ontiveros <juan.ontiveros@zerf.tech>
2024-10-29 18:53:28 -0300

    Fixes for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  eslint: {
+    ignoreDuringBuilds: true
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
