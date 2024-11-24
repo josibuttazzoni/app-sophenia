cfacb498ea47530db9eedb11e65a2f6923eea35e
Santiago Benedetti <santiago.benedetti@theappmaster.com>
2024-11-23 21:41:37 -0300

    fix/fixed martu's comments

--- src/components/ExportWorkOrderModal/DateRangePicker/index.tsx
+++ src/components/ExportWorkOrderModal/DateRangePicker/index.tsx
@@ -1,11 +1,17 @@
 import { cx } from 'class-variance-authority';
 import { es } from 'date-fns/locale';
 import * as React from 'react';
+import { DateRange } from 'react-day-picker';
 
 import { Calendar } from '#components/ui/calendar';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function DateRangePicker({ className, date, setDate }: any) {
+export type DateRangePickerProps = {
+  className?: string;
+  date: DateRange | undefined;
+  setDate: (date: DateRange | undefined) => void;
+};
+
+export function DateRangePicker({ className, date, setDate }: DateRangePickerProps) {
   return (
     <div className={cx('grid h-96 gap-2', className)}>
       <Calendar
