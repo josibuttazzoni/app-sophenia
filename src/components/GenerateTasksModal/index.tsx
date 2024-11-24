33331be93dfacf56a55b4ffbcba576d6ea3e3966
josibuttazzoni <mariajosebuttazzoni@hotmail.com>
2024-11-24 12:38:51 -0300

    feat: add regenerate

--- src/components/GenerateTasksModal/index.tsx
+++ src/components/GenerateTasksModal/index.tsx
@@ -1,4 +1,9 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Backlog, SuggestTasksVariables } from 'src/types/tasks';
+
+import { useSuggestTasks } from '#lib/api/tasks';
+import { useTasksContext } from '#lib/providers/TasksContext';
 
 import EditTasksContent from './EditTasksContent';
 import GenerateTasksContent from './GenerateTasksContent';
@@ -10,9 +15,38 @@ export default function GenerateTasksModal({
 }) {
   const [isEditing, setIsEditing] = useState(false);
 
+  const form = useForm({
+    defaultValues: {
+      objective: '',
+      seasonMoment: ''
+    } as SuggestTasksVariables
+  });
+
+  const { setSuggestedTasks } = useTasksContext(({ setSuggestedTasks }) => ({
+    setSuggestedTasks
+  }));
+
+  const handleSuccess = (tasks: Backlog[]) => {
+    setSuggestedTasks(tasks);
+    setIsEditing(true);
+  };
+
+  const { mutate: suggestTasks, status } = useSuggestTasks(handleSuccess);
+
+  const onSubmit = (data: SuggestTasksVariables) => {
+    suggestTasks({ objective: data.objective, seasonMoment: data.seasonMoment });
+  };
+
+  const suggestLoading = status === 'pending';
+
   return isEditing ? (
-    <EditTasksContent setTasksModalOpen={setTasksModalOpen} />
+    <EditTasksContent
+      suggestLoading={suggestLoading}
+      suggestForm={form}
+      onSubmit={onSubmit}
+      setTasksModalOpen={setTasksModalOpen}
+    />
   ) : (
-    <GenerateTasksContent setIsEditing={setIsEditing} />
+    <GenerateTasksContent suggestLoading={suggestLoading} form={form} onSubmit={onSubmit} />
   );
 }
