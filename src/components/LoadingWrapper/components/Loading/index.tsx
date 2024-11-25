bbf281a9ba14740c292d56992eb6b3a0e5d8844b
Martina Mattioli <martinamattioli0@gmail.com>
2024-11-25 16:20:43 -0300

    fix: change classname

--- src/components/LoadingWrapper/components/Loading/index.tsx
+++ src/components/LoadingWrapper/components/Loading/index.tsx
@@ -13,7 +13,7 @@ export default function Loading({ className, white, small }: LoadingProps) {
   return (
     <div className={cx('flex h-full w-full flex-col items-center justify-center', className)}>
       {small ? (
-        <div className={`${white ? 'text-white' : 'text-disco'}`}>
+        <div className={white ? 'text-white' : 'text-disco'}>
           <Spinner className="h-6 w-6 animate-spin" />
         </div>
       ) : (
