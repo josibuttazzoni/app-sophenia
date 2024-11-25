6cac7be109c9bb8bd6b384a1ea10b0438b27bf46
josibuttazzoni <mariajosebuttazzoni@hotmail.com>
2024-11-24 23:05:20 -0300

    feat: add logout on worker side

--- src/pages/worker/tasks/index.tsx
+++ src/pages/worker/tasks/index.tsx
@@ -1,24 +1,35 @@
 import { cx } from 'class-variance-authority';
+import { deleteCookie } from 'cookies-next';
 import useTranslation from 'next-translate/useTranslation';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 import Clock from '#assets/clock.svg';
 import Logout from '#assets/logout.svg';
 import { STATUS_COLORS } from '#components/BoardColumn/constants';
+import { COOKIES } from '#constants/cookies';
+import { PAGES_PATHS } from '#constants/pages';
 import { ROUTES } from '#constants/routes';
 import { TRANSLATIONS_NAMESPACES } from '#constants/translations';
+import { queryClient } from '#lib/api';
 import { useBoard } from '#lib/api/workOrders/useBoard';
 import { formatDateES } from '#utils/date';
 
 export default function Tasks() {
   const { t } = useTranslation(TRANSLATIONS_NAMESPACES.TASKS);
   const { data } = useBoard();
+  const router = useRouter();
+  const handleLogout = () => {
+    deleteCookie(COOKIES.AUTH_TOKEN);
+    deleteCookie(COOKIES.AUTH_ROLE);
+    queryClient.clear();
+    router.replace(PAGES_PATHS.LOGIN);
+  };
   return (
     <div className="min-h-screen">
       <div className="sticky top-0 flex w-full justify-between border-b border-claret bg-white p-3">
         <div className="text-lg font-medium text-disco">{data?.[0]?.workerAssigned.fullname}</div>
-        {/* TODO: Add logout logic */}
-        <Logout className="[&>path]:stroke-[#821744]" />
+        <Logout onClick={handleLogout} className="[&>path]:stroke-[#821744]" />
       </div>
       <div className="flex h-full w-full flex-col gap-y-3 px-4 pt-3">
         <div className="text-md">
